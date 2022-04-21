38f1e3f0a995c4ceb9524f4d114b55535b50c529
emilianoparrilla <93682192+emilianoparrilla@users.noreply.github.com>
2022-04-21 17:21:07 -0300

    Delete tp1 directory

--- tp1/ejercicio10/testLogin.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'Login.php';
-$objLogin1 = new Login("Emiliano", "emi1993", "Tu nombre más tu año de nacimiento", array("emi1234", "parri1993", "&emiparrilla", "&p1a2r3r4i5"));
-echo $objLogin1;
-//echo "Validación contraseña: \n" . $objLogin1->validar("emi1234");
-echo "Cambio de contraseña: \n" . $objLogin1->cambiarContrasenia("&parrilla28");
-//echo "Olvidé mi contraseña: \n" . $objLogin1->recordar("Emiliano");
--- tp1/ejercicio10/testReloj.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "Reloj.php";
-$objReloj = new Reloj(23,59,00);
-//echo $objReloj . "\n";
-echo $objReloj->incremento();
-$objReloj->puesta_a_cero();
-?>
